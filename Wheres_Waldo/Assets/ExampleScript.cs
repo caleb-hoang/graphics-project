7d68f8dca2d052500402e540459c7d0f3edccb28
Jimjonjoe <94248446+Jimjonjoe@users.noreply.github.com>
2024-03-16 19:08:42 -0500

    Create Project
    Created and uploaded the Unity files to be used for the project
    
    Currently, when played, it creates a pair of cubes. One can be rotated using the arrow keys.

---
+++ Wheres_Waldo/Assets/ExampleScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript: MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle;
+    private GameObject cube1, cube2;
+    public bool isFrame, isPause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube1.transform.position = new Vector3(0.75f, 0.0f, 0.0f);
+        cube1.transform.Rotate(90.0f, 0.0f, 0.0f, Space.World);
+        cube1.GetComponent<Renderer>().material.color = Color.yellow;
+        cube1.name = "Self";
+
+        cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube2.transform.position = new Vector3(-0.75f, 0.0f, 0.0f);
+        cube2.transform.Rotate(90.0f, 0.0f, 0.0f, Space.World);
+        cube2.GetComponent<Renderer>().material.color = Color.cyan;
+        cube2.name = "World";
+
+        xAngle = 1;
+        yAngle = 1;
+        zAngle = 0;
+        isPause = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isFrame && !isPause) { 
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                cube1.transform.Rotate(1, 0, 0, Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                cube1.transform.Rotate(-1, 0, 0, Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow)) {
+                cube1.transform.Rotate(0, 1, 0, Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                cube1.transform.Rotate(0, -1, 0, Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.Space)) {
+                cube2.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+            }
+        }
+        isFrame = !isFrame;
+    }
+}
