7aa4baa93c652bb7e3d450cfb685aa66749c7685
Anthony <85533216+Antoneee@users.noreply.github.com>
2024-04-13 23:55:52 -0500

    Add 3 more levels and display current level in UI

---
+++ Wheres_Waldo/Assets/UI/LevelBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelBox : MonoBehaviour
+{
+    public Text level;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        string levelNumberStr = currentSceneName.Split('_')[1];
+        level.text = "Level " + levelNumberStr;
+    }
+}
