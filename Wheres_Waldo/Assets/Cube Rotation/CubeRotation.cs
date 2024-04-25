4c47c61086f68a9adeba6aa864bb93d277b0142c
Anthony <85533216+Antoneee@users.noreply.github.com>
2024-04-24 22:09:25 -0500

    Added smooth rotation to the cube

--- Wheres_Waldo/Assets/Cube Rotation/CubeRotation.cs
+++ Wheres_Waldo/Assets/Cube Rotation/CubeRotation.cs
@@ -1,19 +1,56 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeRotation : MonoBehaviour
 {
+    public float rotationSpeed = 90f; // Adjust the speed of rotation here
+
+    private bool isRotating = false;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            transform.Rotate(Vector3.up, 90, Space.World);
-        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            transform.Rotate(Vector3.up, 90, Space.World);
-        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            transform.Rotate(Vector3.right, 90, Space.World);
-        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            transform.Rotate(Vector3.right, 90, Space.World);
+        if (!isRotating)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                StartCoroutine(Rotate(Vector3.up, 90));
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                StartCoroutine(Rotate(Vector3.up, -90));
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                StartCoroutine(Rotate(Vector3.right, -90));
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                StartCoroutine(Rotate(Vector3.right, 90));
+            }
         }
+    }
+
+    IEnumerator Rotate(Vector3 axis, float angle)
+    {
+        isRotating = true;
+
+        Quaternion fromRotation = transform.rotation;
+        Quaternion toRotation = Quaternion.Euler(axis * angle) * transform.rotation;
+
+
+        float elapsedRotationTime = 0f;
+        float totalRotationTime = 0.5f;
+
+        while (elapsedRotationTime < totalRotationTime)
+        {
+            float t = elapsedRotationTime / totalRotationTime;
+            transform.rotation = Quaternion.Slerp(fromRotation, toRotation, t);
+            elapsedRotationTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = toRotation;
+
+        isRotating = false;
     }
 }
